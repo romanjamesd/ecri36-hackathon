694f9a0fac91cf03cccde668eecd6d97b25889b5
Roman <rdarker@gmail.com>
2022-09-28 21:56:00 -0400

    don't think anything I wrote for this commit works : )

--- addHuaAndRoman.js
+++ addHuaAndRoman.js
@@ -1,13 +1,27 @@
-const huaAndRoman = document.createElement('img');
-huaAndRoman.src = 'hua-and-roman.png';
-huaAndRoman.className = 'handsomeFellas';
-huaAndRoman.style.position = 'fixed';
-huaAndRoman.style.top = '0';
-huaAndRoman.style.left = '0';
-huaAndRoman.style.zIndex = '100';
-huaAndRoman.widthNumber = 50;
-huaAndRoman.style.width = `${this.widthNumber}px`;
+const makeHuaRomanElement = () => {
+  const huaAndRoman = document.createElement('img');
+  huaAndRoman.src = 'ecri36-hackathon/hua-and-roman.png';
+  huaAndRoman.className = 'handsomeFellas';
+  huaAndRoman.style.zIndex = '100';
+  huaAndRoman.style.maxWidth = `100%`;
+  return huaAndRoman;
+}
+document.onload(() => {
+  //use func to create element
+  const ourPic = makeHuaRomanElement();
+  //collect all img elements
+  const images = document.querySelectorAll('img');
+  //replace each with pic of us
+  for (let image of images){
+    image.replaceWith(ourPic);
+  }
+  
+});
 
-
-//append to body of html doc
-document.querySelector('body').appendChild(huaAndRoman);
+  
+//   huaAndRoman.grow = () => {
+//     //convert, intrement, and revert width
+//     const stringVal = this.style.width;
+//     let newVal = parseInt(stringVal.slice(0, stringVal.length - 2)) + 50;
+//     this.style.width = newVal + 'px';
+//   }
--- main.js
+++ main.js
@@ -1,10 +1,70 @@
-goBackToWork(document.body)
 
-function goBackToWork(el) {
-    if(el.hasChildNodes()) {
-        el.childNodes.forEach(replaceText)
-    } else if (el.nodeType === Text.TEXT_NODE) {
-        el.textContent = 'GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! GO BACK TO CODING! '
+// function goBackToCode() {
+//     if(!document.body.innerHTML.includes('javascript')) {
+//         document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')
+//     }
+    // }
+    // if(!document.body.innerHTML.includes('code')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
         
-    }
+    // }
+    // if(!document.body.innerHTML.includes('coding')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
+        
+    // }
+    // if(!document.body.innerHTML.includes('java')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
+         
+    // }
+    // if(!document.body.innerHTML.includes('python')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
+         
+    // }
+    // if(!document.body.innerHTML.includes('algorithm')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
+         
+    // }
+    // if(!document.body.innerHTML.includes('array')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
+         
+    // }
+    // if(!document.body.innerHTML.includes('object')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
+         
+    // }
+    // if(!document.body.innerHTML.includes('function')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
+        
+    // }
+    // if(!document.body.innerHTML.includes('html')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
+        
+    // }
+    // if(!document.body.innerHTML.includes('HTML')) {
+    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
+        
+//     // }
+    
+// }
+
+// goBackToCode();
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('this script loads');
+  //use func to create element
+  const ourPic = makeHuaRomanElement();
+  //collect all img elements
+  const images = document.querySelectorAll('img');
+  //replace each with pic of us
+  for (let image of images){
+    image.replaceWith(ourPic);
+  }
+  
+});
+const makeHuaRomanElement = () => {
+  const huaAndRoman = document.createElement('img');
+  huaAndRoman.src = 'ecri36-hackathon/hua-and-roman.png';
+  huaAndRoman.className = 'handsomeFellas';
+  huaAndRoman.style.zIndex = '100';
+  huaAndRoman.style.maxWidth = `100%`;
+  return huaAndRoman;
 }
