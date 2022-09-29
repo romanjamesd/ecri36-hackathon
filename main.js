c0b04229c157190e087161cb0d943a99d183b58d
Roman <rdarker@gmail.com>
2022-09-29 18:50:30 -0400

    last

--- main.js
+++ main.js
@@ -1,70 +1,35 @@
-
-// function goBackToCode() {
-//     if(!document.body.innerHTML.includes('javascript')) {
-//         document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')
-//     }
-    // }
-    // if(!document.body.innerHTML.includes('code')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
-        
-    // }
-    // if(!document.body.innerHTML.includes('coding')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
-        
-    // }
-    // if(!document.body.innerHTML.includes('java')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
-         
-    // }
-    // if(!document.body.innerHTML.includes('python')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
-         
-    // }
-    // if(!document.body.innerHTML.includes('algorithm')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
-         
-    // }
-    // if(!document.body.innerHTML.includes('array')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
-         
-    // }
-    // if(!document.body.innerHTML.includes('object')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')  
-         
-    // }
-    // if(!document.body.innerHTML.includes('function')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
-        
-    // }
-    // if(!document.body.innerHTML.includes('html')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
-        
-    // }
-    // if(!document.body.innerHTML.includes('HTML')) {
-    //     document.body.innerHTML = document.body.innerHTML.replaceAll('', 'GO BACK TO CODING BRO ')   
-        
-//     // }
-    
-// }
-
-// goBackToCode();
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('this script loads');
-  //use func to create element
-  const ourPic = makeHuaRomanElement();
-  //collect all img elements
-  const images = document.querySelectorAll('img');
-  //replace each with pic of us
-  for (let image of images){
-    image.replaceWith(ourPic);
+function goBackToCode() {
+	if (
+		document.body.innerHTML.includes("Javascript") ||
+		document.body.innerHTML.includes("JavaScript") ||
+		document.body.innerHTML.includes("javascript") ||
+		document.body.innerHTML.includes("HTML") ||
+		document.body.innerHTML.includes("CSS")
+	) {
+    console.log('Found Something!')
+  } else {
+    console.log("No Javascript, HTML, or CSS Found!")
+    document.body.innerHTML = document.body.innerHTML.replaceAll("", "GO BACK TO CODING BRO ");
   }
-  
-});
-const makeHuaRomanElement = () => {
-  const huaAndRoman = document.createElement('img');
-  huaAndRoman.src = 'ecri36-hackathon/hua-and-roman.png';
-  huaAndRoman.className = 'handsomeFellas';
-  huaAndRoman.style.zIndex = '100';
-  huaAndRoman.style.maxWidth = `100%`;
-  return huaAndRoman;
 }
+goBackToCode();
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	console.log("this script loads");
+// 	//use func to create element
+// 	const ourPic = makeHuaRomanElement();
+// 	//collect all img elements
+// 	const images = document.querySelectorAll("img");
+// 	//replace each with pic of us
+// 	for (let image of images) {
+// 		image.replaceWith(ourPic);
+// 	}
+// });
+// const makeHuaRomanElement = () => {
+// 	const huaAndRoman = document.createElement("img");
+// 	huaAndRoman.src = "ecri36-hackathon/huaRoman1.png";
+// 	huaAndRoman.className = "handsomeFellas";
+// 	huaAndRoman.style.zIndex = "100";
+// 	huaAndRoman.style.maxWidth = `100%`;
+// 	return huaAndRoman;
+// };
